f58078cda4eb45dfe731be7fe67922701ac48557
ellijs <ellijs@hotmail.com>
2021-07-01 17:55:09 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+function DOMContentLoaded() {
+  document.getElementById("text").textContent = "This is really cool!";
+}
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  DOMContentLoaded(e);
+});
